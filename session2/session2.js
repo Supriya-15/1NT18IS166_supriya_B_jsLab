9b7172572d72a1dece41c7260dac41ae93ba467f
Supriya-15 <55291168+Supriya-15@users.noreply.github.com>
2021-05-23 08:22:06 +0530

    Add files via upload

---
+++ session2/session2.js
@@ -0,0 +1,12 @@
+ <script >
+        function myfunction(){
+        var x=document.querySelectorAll(".wall");
+        var i;
+        for(i=0;i<5;i++){
+
+        
+
+        x[i].style.backgroundColor="blue";
+       // document.body.append(x);
+}
+        }
